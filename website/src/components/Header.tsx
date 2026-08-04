ac923ecc84adb9198e7b959449040acd870052a0
harovud197-code <harovud197@gmail.com>
2026-08-04 16:07:23 +0800

    Add Case Studies link to site navigation

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -32,6 +32,7 @@ export default function Header() {
           </details>
           <Link href="/#custom-socks">OEM</Link>
           <Link href="/blog">Blog</Link>
+          <Link href="/case-studies/uk-football-brand-growth" className="whitespace-nowrap">Case Studies</Link>
           <Link href="/sports-socks-moq-guide" className="whitespace-nowrap">MOQ Guide</Link>
           <Link href="/#inquiry">Contact</Link>
         </nav>
@@ -50,3 +51,4 @@ export default function Header() {
     </header>
   );
 }
+
